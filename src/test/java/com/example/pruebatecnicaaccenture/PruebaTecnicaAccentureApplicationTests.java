180693d4c339fb8541a8cfd70603b59a99cf9e67
Deiby Bustos <deibybustos11@gmail.com>
2026-04-23 23:51:56 -0500

    Configuracion Inicial Spring Boot

---
+++ src/test/java/com/example/pruebatecnicaaccenture/PruebaTecnicaAccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebatecnicaaccenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaAccentureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
